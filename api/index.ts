3239e429272ca6280b434dfbebf8fc3eb4369b8a
Fernando <fernandoeqp59@gmail.com>
2025-07-11 11:20:45 -0300

    okokok

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import { createServer } from "http";
+import { Request, Response } from "express-serve-static-core";
 import app from "../src/server";
 
-export default function handler(req, res) {
+export default function handler(req: Request, res: Response) {
   return app(req, res);
 }

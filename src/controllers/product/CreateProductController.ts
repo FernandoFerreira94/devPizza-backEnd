b5348bb9807828f21cdf789c79c00fa6edc54839
Fernando <fernandoeqp59@gmail.com>
2025-07-11 12:14:08 -0300

    ok

--- src/controllers/product/CreateProductController.ts
+++ src/controllers/product/CreateProductController.ts
@@ -4,54 +4,25 @@ import { CreateProductService } from "../../services/product/CreateProductServic
 import { UploadedFile } from "express-fileupload";
 import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 class CreateProductController {
   async handle(req: Request, res: Response) {
     // descontruindo valores enviado do body
     const { name, price, description, category_id } = req.body;
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-      throw new Error("Erro ao carregar a imagem");
-    } else {
-      // descontruindo valores enviado do body
-      const file: UploadedFile = req.files["file"];
+    const createProductService = new CreateProductService();
 
-      const createProductService = new CreateProductService();
+    const menu = await createProductService.execute({
+      name,
+      price,
+      description,
+      banner: "",
+      category_id,
+    });
 
-      const resultFile: UploadApiResponse = await new Promise(
-        (resolve, reject) => {
-          cloudinary.uploader
-            .upload_stream({}, function (error, result) {
-              if (error) {
-                reject();
-                return;
-              }
-
-              resolve(result);
-            })
-
-            .end(file.data);
-        }
-      );
-
-      const menu = await createProductService.execute({
-        name,
-        price,
-        description,
-        banner: resultFile.url,
-        category_id,
-      });
-
-      res.json(menu);
-    }
-
-    return;
+    res.json(menu);
   }
+
+  return;
 }
 
 export { CreateProductController };
